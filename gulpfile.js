e8ae08f7102ddfa7e0eb244bf1445ae44ba9e0df
Eugene <gaster334@gmail.com>
2020-08-29 15:23:46 +0300

    update gulpfile, add fonts and images

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,14 @@
 const 
-    {series, parallel} = require('gulp')
+    {series, parallel} = require('gulp'),
     clean = require('./tasks/del'),
-    styles = require('./tasks/styles')
-    scripts = require('./tasks/scripts')
-    pages = require('./tasks/pages')
+    styles = require('./tasks/styles'),
+    scripts = require('./tasks/scripts'),
+    pages = require('./tasks/pages'),
+    fonts = require('./tasks/fonts'),
+    images = require('./tasks/images'),
     browser = require('./tasks/browser')
 
-const dev = parallel(pages, styles, scripts)
+const dev = parallel(pages, styles, scripts, fonts, images)
 const build = series(clean, dev)
 
 module.exports.dev = series(build, browser)
